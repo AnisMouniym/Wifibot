f42881fed3677ae1fb3f2bbf6379986ecae14d95
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-05-14 07:51:55 +0200

    connect button stylesheet

--- Wifibot/mainwindow.cpp
+++ Wifibot/mainwindow.cpp
@@ -7,10 +7,16 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     Robot = new MyRobot();
+    this->setFixedSize(800,400);
     ui->setupUi(this);
+
     _connect = new QPushButton("Disconnected", this);
-    _connect->setStyleSheet("background-color: red; border-style: outset; border-radius: 10px; color: black");
-    _connect->setGeometry(QRect(200,10,300,20));
+    _connect->setStyleSheet("QPushButton { background-color: red; font: bold 15px; border-radius: 15px; color: black}"
+                            "QPushButton:pressed {background-color: rgb(200, 0, 0); border-style: inset;}");
+    _connect->setGeometry(QRect(250,10,300,35));
+
+
+
 
     connect(_connect, SIGNAL(clicked()), this, SLOT(checkConnection()));
     connect(Robot, SIGNAL(updateUI(QByteArray)), this, SLOT(updateWindows(QByteArray)));
@@ -19,6 +25,7 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete Robot;
 }
 
 
@@ -28,13 +35,15 @@ void MainWindow::checkConnection()
     if(!Robot->isConnected)
     {
         if(Robot->doConnect()){
-            _connect->setStyleSheet("background-color: red; border-style: outset; border-radius: 10px; color: black");
+            _connect->setStyleSheet("QPushButton { background-color: red; border-style: none; padding: 40px ; font: bold 20px; border-radius: 10px; color: black}"
+                                    "QPushButton:pressed {background-color: rgb(200, 0, 0); border-style: inset;}");
     }
     }
     else
     {
         Robot->disConnect();
         _connect->setText("Connected");
-        _connect->setStyleSheet("background-color: green; border-style: outset; border-radius: 10px; color: black");
+        _connect->setStyleSheet("QPushButton { background-color: green; border-style: none; padding: 40px ; font: bold 20px; border-radius: 10px; color: black}"
+                                "QPushButton:pressed {background-color: rgb(200, 0, 0); border-style: inset;}");
     }
 }
